03430eeea50e08d06ef752f61795d915e0f4710f
Mateo-Paternina-Mercado <185979424+Mateo-Paternina-Mercado@users.noreply.github.com>
2024-11-07 16:48:09 -0500

    Exercise 7

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
-import random
+def determinar_signo(numero):
+    if numero > 0:
+        print("The number is positive.")
+    elif numero < 0:
+        print("The number is negative.")
+    else:
+        print("The number is zero.")
 
-def juego_adivina_numero():
-    print("Welcome to the Guess the Number game!")
-    numero_secreto = random.randint(1, 10)
-    intentos = 0
-
-    while True:
-        try:
-            # Pedimos al jugador que introduzca un número
-            adivinanza = int(input("Guess the number (between 1 and 10): "))
-            intentos += 1
-            
-            # Verificamos si el número es mayor, menor o igual al número secreto
-            if adivinanza < numero_secreto:
-                print("The number is greater. Try again!")
-            elif adivinanza > numero_secreto:
-                print("The number is less. Try again!")
-            else:
-                print(f"Congratulations! Have you guessed the number in {intentos} attempts.")
-                break
-        except ValueError:
-            print("Please enter a valid number.")
-
-# Inicia el juego
-juego_adivina_numero()
+# Solicitar al usuario que ingrese un número
+numero = float(input("Enter a number: "))
+determinar_signo(numero)
