0f14ac2f55d6e0c954cd513edbadd394c1168fdc
Mateo-Paternina-Mercado <185979424+Mateo-Paternina-Mercado@users.noreply.github.com>
2024-11-07 17:04:52 -0500

    Exercise 5

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-def determinar_signo(numero):
-    if numero > 0:
-        print("The number is positive.")
-    elif numero < 0:
-        print("The number is negative.")
-    else:
-        print("The number is zero.")
+def imprimir_dia_semana(numero):
+    match numero:
+        case 1:
+            print("Monday")
+        case 2:
+            print("Tuesday")
+        case 3:
+            print("Wednesday")
+        case 4:
+            print("Thursday")
+        case 5:
+            print("Friday")
+        case 6:
+            print("Saturday")
+        case 7:
+            print("Sunday")
+        case _:
+            print("Invalid number. Please enter a number from 1 to 7.")
 
-# Solicitar al usuario que ingrese un número
-numero = float(input("Enter a number: "))
-determinar_signo(numero)
+# Solicitar al usuario que ingrese un número del 1 al 7
+numero = int(input("Enter a number from 1 to 7: "))
+imprimir_dia_semana(numero)
