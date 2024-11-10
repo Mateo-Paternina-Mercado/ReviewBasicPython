8b18579559bd8e237235300baad56594175a31a4
Mateo-Paternina-Mercado <185979424+Mateo-Paternina-Mercado@users.noreply.github.com>
2024-11-10 10:21:47 -0500

    Exercise 14

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-def mayor_de_tres():
-    # Solicitar tres números al usuario
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    num3 = float(input("Enter the third number: "))
+def adivinanza_letra():
+    # Letra secreta
+    letra_secreta = "A"
+    
+    # Solicitar la letra al usuario
+    letra_usuario = input("Guess the secret letter: ").upper()
+    
+    # Usar match para comparar la letra ingresada
+    match letra_usuario:
+        
+        case _:
+            if letra_usuario == letra_secreta:
+                print("Congratulation, you guessed the letter")
+            else:           
+                print("Sorry, that's not the correct letter. Try again.")
+        
 
-    # Determinar el mayor de los tres números
-    if num1 >= num2 and num1 >= num3:
-        mayor = num1
-    elif num2 >= num1 and num2 >= num3:
-        mayor = num2
-    else:
-        mayor = num3
-
-    # Mostrar el número mayor
-    print(f"The largest number is: {mayor}.")
-
-# Llamar a la función para determinar el mayor de los tres números
-mayor_de_tres()
+# Llamar a la función para iniciar el juego
+adivinanza_letra()
